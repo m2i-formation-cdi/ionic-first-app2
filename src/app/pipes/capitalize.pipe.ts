d7f08a42e6bf4ecffaa649c87d4fd20284d4c263
smaloron <secret@pastesoignons.com>
2019-09-04 10:05:45 +0200

    pipe capitalize suite et fin

--- src/app/pipes/capitalize.pipe.ts
+++ src/app/pipes/capitalize.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizePipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return "Hello";
+  transform(value: string, ...args: any[]): any {
+    return  value .split(" ")
+                  .map(word => this.capitalize(word))
+                  .join(" ");
+  }
+
+  private capitalize(word){
+    return  word.charAt(0).toUpperCase() 
+          + word.substring(1).toLowerCase();
   }
 
 }
